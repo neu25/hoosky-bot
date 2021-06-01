3d9d8f6cbf3311e1a410c0d68f10d6c988951cf1
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2021-06-01 19:19:55 -0400

    Skeletal Plan for starboard
    Pushing this now as a way for others to review my planning as well as me to now use aiyans updated code

---
+++ src/commands/starboard/setstarboard.ts
@@ -0,0 +1,24 @@
+import Command from '../Command';
+
+const setstarboard = new Command({
+  name: 'setstarboard',
+  description: 'minstars:(interger), channel:(string)',
+  handler: async ctx => {
+
+  /*
+    Use response system to extract variables from parameters
+    Use database as a means to communicate with starboard.ts and as a means to store afformentioned variables
+    Implement/Utilize a listening system to recognize when the number of stars has been reached
+      Once code works, revamp setstarboard to include self-star:(boolean), staring a bot(boolean), and emoji(STRING)
+  */
+
+
+
+
+
+
+
+
+
+  }
+}
