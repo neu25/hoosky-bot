b2db136aa9bc7731d4bd8d664f849d1defd35fd9
ezgibas <52749796+ezgibas@users.noreply.github.com>
2021-06-13 15:36:49 -0400

    Updated skeleton for starboard

--- src/commands/starboard/setstarboard.ts
+++ src/commands/starboard/setstarboard.ts
@@ -1,24 +1,47 @@
-import Command from '../Command';
+import * as Discord from '../../Discord';
+import Command from '../../Command'; 
+import SubCommand from '../../SubCommand';
+import CommandOption from '../../CommandOption';
 
 const setstarboard = new Command({
   name: 'setstarboard',
-  description: 'minstars:(interger), channel:(string)',
-  handler: async ctx => {
+  description: 'Manage starboards',
+  options: [
+    new SubCommand({
+      name: 'configure',
+      displayName: 'Configure',
+      description: 'Configure a channel as a new starboard',
+      requiredPermissions: [Discord.Permission.MANAGE_CHANNELS],
+      options: [
+        new CommandOption({
+          name: 'channel',
+          description: 'Choose the new starboard channel',
+          required: true,
+          type: Discord.CommandOptionType.CHANNEL,
+        }),
+        new CommandOption({
+          name: 'emoji',
+          description: "Choose new board's emoji",
+          required: true,
+          type: Discord.CommandOptionType.STRING,
+        }),
+        new CommandOption({
+          name: 'minstars',
+          description: "Minimum stars required to get on the board",
+          required: true,
+          type: Discord.CommandOptionType.INTEGER,
+        }),
+      ],
+      handler: async ctx => {
+        
+      /*
+        Use response system to extract variables from parameters
+        Use database as a means to communicate with starboard.ts and as a means to store afformentioned variables
+        Implement/Utilize a listening system to recognize when the number of stars has been reached
+          Once code works, revamp setstarboard to include self-star:(boolean), starring a bot(boolean), and emoji(STRING)
+      */
 
-  /*
-    Use response system to extract variables from parameters
-    Use database as a means to communicate with starboard.ts and as a means to store afformentioned variables
-    Implement/Utilize a listening system to recognize when the number of stars has been reached
-      Once code works, revamp setstarboard to include self-star:(boolean), staring a bot(boolean), and emoji(STRING)
-  */
-
-
-
-
-
-
-
-
-
-  }
-}
+      },
+    }),
+  ],
+});
