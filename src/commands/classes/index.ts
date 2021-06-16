459c8e4174961f53b23cd8b46cc6f83b4428ac80
SamHaggans <sam@samhaggans.com>
2021-06-16 00:52:09 -0600

    Implement creating classes and adding to db

---
+++ src/commands/classes/index.ts
@@ -0,0 +1,1 @@
+export { default as newclass } from './newclass';
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,14 @@
 import ping from './ping';
 import poll from './poll';
 import { mute, unmute } from './mute';
+import { newclass } from './classes';
 
 const commands = {
   ping,
   mute,
   unmute,
   poll,
+  newclass
 };
 
 export default commands;
