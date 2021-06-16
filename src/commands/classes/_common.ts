8afbc0cc7c4059032908159f8e8f79f95500de64
SamHaggans <sam@samhaggans.com>
2021-06-16 17:38:10 -0600

    Add listing of classes and class members

--- src/commands/classes/_common.ts
+++ src/commands/classes/_common.ts
@@ -1,10 +1,12 @@
 import ExecutionContext from '../../ExecutionContext';
 import * as Discord from '../../Discord';
+import { Cursor } from 'mongodb';
 
 export type dbClass = {
   name: string,
   description: string
-  id: string
+  id: string,
+  members: string[]
 };
 
 export const classExists = async (
@@ -35,9 +37,53 @@ export const createClass = async (
   ctx: ExecutionContext,
   guildId: string,
   classObject: dbClass
-) => {
+): Promise<any> => {
   const db = await ctx.db.getDb(guildId);
 
   const classObj = await db.collection('classes').insertOne(classObject);
   return classObj;
 };
+
+export const addUserToClass = async (
+  ctx: ExecutionContext,
+  guildId: string,
+  userId: string,
+  className: string
+) => {
+  const db = await ctx.db.getDb(guildId);
+
+
+  await db.collection('classes').update({
+    name: className
+  },
+  {$push: {
+    members: userId
+  }}
+  );
+};
+
+export const getClassMembers = async (
+  ctx: ExecutionContext,
+  guildId: string,
+  className: string
+): Promise <string[]> => {
+  const db = await ctx.db.getDb(guildId);
+
+
+  const classObject = await db.collection('classes').findOne({name: className});
+
+  return classObject.members;
+};
+
+export const getClasses = async (
+  ctx: ExecutionContext,
+  guildId: string
+): Promise<any> => {
+  const db = await ctx.db.getDb(guildId);
+
+
+  const classes = await db.collection('classes').find();
+
+  return classes;
+};
+
