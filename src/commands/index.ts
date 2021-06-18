5c64daca006b3845a7a3c4a0ca8a563c5209de2b
Ezgi Baş <basezgi@gmail.com>
2021-06-17 21:48:49 -0400

    starboard counts reactions

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,13 @@ import ping from './ping';
 import poll from './poll';
 import mute from './mute';
 import course from './course';
-
+import setstarboard from './starboard/setstarboard';
 const commands = {
   ping,
   mute,
   poll,
   course,
+  setstarboard,
 };
 
 export default commands;
--- src/triggers/MESSAGE_REACTION_ADD/starboard.ts
+++ src/triggers/MESSAGE_REACTION_ADD/starboard.ts
@@ -1,11 +1,24 @@
 import * as Discord from '../../Discord';
 import Trigger from '../../Trigger';
+import setstarboard from '../../commands/starboard/setstarboard'
 
+/*
+this only works for star emojis for now, ideally we will have multiple boards with multiple emojis and minstars
+*/
 const starboard = new Trigger<Discord.Event.MESSAGE_REACTION_ADD>({
   event: Discord.Event.MESSAGE_REACTION_ADD,
-  handler: ctx => {
+  handler: async ctx => {
+    const minstars = 1; //1 for now, ideally will get this value from setstarboard
+    //get information about the reaction
     const data = ctx.getData();
-    console.log(data);
+    console.log("a message has been starred"); //DEBUG, write data of reaction in console 
+    if(data.emoji.name === '⭐') {
+    //get array of users who reacted with a star
+    const reactions = await ctx.api.getReactions(data.message_id, data.channel_id, data.emoji.name);
+    if(reactions.length >= minstars){
+      
+    }
+    }
   },
 });
 
