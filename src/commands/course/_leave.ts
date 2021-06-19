0902041a41789aee9601883cd39a1ca75588f0f8
SamHaggans <sam@samhaggans.com>
2021-06-19 01:24:56 -0600

    Add leave and remove course commands

---
+++ src/commands/course/_leave.ts
@@ -0,0 +1,57 @@
+import * as Discord from '../../Discord';
+import Command from '../../Command';
+import SubCommand from '../../SubCommand';
+import CommandOption from '../../CommandOption';
+import { CommandOptionType } from '../../Discord';
+import {
+  courseExists,
+  createCourse,
+  getCourse,
+  addUserToCourse,
+  getCourseMembers,
+  getCourses,
+  removeUserFromCourse,
+} from './_common';
+
+export const leave = new SubCommand({
+    name: 'leave',
+    displayName: 'Leave Course',
+    description: 'Leaves a course',
+    requiredPermissions: [],
+    options: [
+      new CommandOption({
+        name: 'role',
+        description: 'The course role',
+        required: true,
+        type: CommandOptionType.ROLE,
+      }),
+    ],
+    handler: async ctx => {
+      const guildId = ctx.mustGetGuildId();
+      const roleId = ctx.getArgument<Discord.CommandOptionType.ROLE>('role') as unknown as string;
+      if (!(await courseExists(ctx, guildId, roleId))) {
+        ctx.respondWithError(`That course does not exist`);
+      } else {
+        const userId = ctx.interaction.member?.user?.id;
+
+        if (userId) {
+          if (
+            (await (
+              await getCourseMembers(ctx, guildId, roleId)
+            ).includes(userId))
+          ) {
+            await ctx.api.removeRoleFromMember(guildId, userId, roleId);
+
+            await removeUserFromCourse(ctx, guildId, userId, roleId);
+
+            const course = await getCourse(ctx, guildId, roleId);
+
+            // Notify of successful course creation
+            return ctx.respondWithMessage(`Left course **${course.crn} - ${course.name}**`);
+          } else {
+            ctx.respondWithError(`You aren't in that course`);
+          }
+        }
+      }
+    },
+});
--- src/commands/course/course.ts
+++ src/commands/course/course.ts
@@ -4,6 +4,8 @@ import { create } from './_create';
 import { join } from './_join';
 import { list } from './_list';
 import { roster } from './_roster';
+import { leave } from './_leave';
+import { remove } from './_remove';
 
 const course = new Command({
   name: 'course',
@@ -13,6 +15,8 @@ const course = new Command({
     join,
     list,
     roster,
+    leave,
+    remove
   ],
 });
 
