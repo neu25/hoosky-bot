c8e152c4f5cdb9d54c58800f4989d934308c8d31
SamHaggans <sam@samhaggans.com>
2021-06-19 00:47:48 -0600

    Separate commands into invidual modules and reference courses by role

---
+++ src/commands/course/_join.ts
@@ -0,0 +1,56 @@
+import * as Discord from '../../Discord';
+import Command from '../../Command';
+import SubCommand from '../../SubCommand';
+import CommandOption from '../../CommandOption';
+import { CommandOptionType } from '../../Discord';
+import {
+  courseExists,
+  createCourse,
+  getCourse,
+  addUserToCourse,
+  getCourseMembers,
+  getCourses,
+} from './_common';
+
+export const join = new SubCommand({
+    name: 'join',
+    displayName: 'Join Course',
+    description: 'Joins a course',
+    requiredPermissions: [],
+    options: [
+      new CommandOption({
+        name: 'role',
+        description: 'The course role',
+        required: true,
+        type: CommandOptionType.ROLE,
+      }),
+    ],
+    handler: async ctx => {
+      const guildId = ctx.mustGetGuildId();
+      const roleId = ctx.getArgument<Discord.CommandOptionType.ROLE>('role') as unknown as string;
+      if (!(await courseExists(ctx, guildId, roleId))) {
+        ctx.respondWithError(`That course does not exist`);
+      } else {
+        const userId = ctx.interaction.member?.user?.id;
+
+        if (userId) {
+          if (
+            !(await (
+              await getCourseMembers(ctx, guildId, roleId)
+            ).includes(userId))
+          ) {
+            await ctx.api.addRoleToMember(guildId, userId, roleId);
+
+            await addUserToCourse(ctx, guildId, userId, roleId);
+
+            const course = await getCourse(ctx, guildId, roleId);
+
+            // Notify of successful course creation
+            return ctx.respondWithMessage(`Joined course **${course.name}**`);
+          } else {
+            ctx.respondWithError(`You are already in that course`);
+          }
+        }
+      }
+    },
+});
---
+++ src/commands/course/_list.ts
@@ -0,0 +1,34 @@
+import * as Discord from '../../Discord';
+import Command from '../../Command';
+import SubCommand from '../../SubCommand';
+import CommandOption from '../../CommandOption';
+import { CommandOptionType } from '../../Discord';
+import {
+    courseExists,
+    createCourse,
+    getCourse,
+    addUserToCourse,
+    getCourseMembers,
+    getCourses,
+} from './_common';
+
+export const list = new SubCommand({
+    name: 'list',
+    displayName: 'List Courses',
+    description: 'Lists all available courses',
+    requiredPermissions: [],
+    options: [],
+    handler: async ctx => {
+      const guildId = ctx.mustGetGuildId();
+      let coursesList = 'Here is a list of courses: \n';
+      coursesList += '```';
+      const courses = await getCourses(ctx, guildId);
+      while (await courses.hasNext()) {
+        const nextCourse = await courses.next();
+        coursesList += `${nextCourse.name} - ${nextCourse.description} \n`;
+      }
+      coursesList += '```';
+      ctx.respondSilently(coursesList);
+    },
+});
+  
