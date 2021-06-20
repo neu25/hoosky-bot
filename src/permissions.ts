246305eb7a72b40488b2e250997b92dd42cc8405
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2021-06-20 19:42:42 -0400

    Remove isSuperset

--- src/permissions.ts
+++ src/permissions.ts
@@ -12,17 +12,6 @@ export const isSubset = (p1: number, p2: number): boolean => {
 };
 
 /**
- * Returns whether `p1` is a superset of `p2`. That is, the every permission in
- * `p2` is present in `p1`.
- *
- * @param p1 The parent permissions.
- * @param p2 the child permissions
- */
-export const isSuperset = (p1: number, p2: number): boolean => {
-  return isSubset(p2, p1);
-};
-
-/**
  * Returns whether `perms` has the permission `p`.
  *
  * @param perms The permissions to test.
