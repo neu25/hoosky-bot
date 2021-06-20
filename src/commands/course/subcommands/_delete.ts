91fee0297ce9487edb0f6b3d25d9272ec9e6354a
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2021-06-20 09:21:09 -0400

    Fix IDs to use both subject and number

--- src/commands/course/subcommands/_delete.ts
+++ src/commands/course/subcommands/_delete.ts
@@ -1,7 +1,7 @@
 import * as Discord from '../../../Discord';
 import SubCommand from '../../../SubCommand';
 import CommandOption from '../../../CommandOption';
-import { getCourse, deleteCourse, boldCourse } from '../_common';
+import { getCourseByRoleId, deleteCourse, boldCourse } from '../_common';
 
 export const del = new SubCommand({
   name: 'delete',
@@ -19,7 +19,7 @@ export const del = new SubCommand({
   handler: async ctx => {
     const guildId = ctx.mustGetGuildId();
     const roleId = ctx.getArgument<string>('role') as string;
-    const course = await getCourse(ctx, guildId, roleId);
+    const course = await getCourseByRoleId(ctx, guildId, roleId);
 
     if (!course) {
       return ctx.respondWithError('That course does not exist');
