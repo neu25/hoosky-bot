60e7441f7a43531f89e384a57ef336ef74687a0f
SamHaggans <sam@samhaggans.com>
2021-06-21 20:38:27 -0400

    Add list-joined to courses to list currently joined courses for self or other user

--- src/commands/course/subcommands/index.ts
+++ src/commands/course/subcommands/index.ts
@@ -2,9 +2,10 @@ import { create } from './_create';
 import { del } from './_delete';
 import { join } from './_join';
 import { leave } from './_leave';
-import { list } from './_list';
+import { listAll } from './_listAll';
+import { listJoined } from './_listJoined';
 import { roster } from './_roster';
 
-export const subcommands = [create, del, join, leave, list, roster];
+export const subcommands = [create, del, join, leave, listAll, listJoined, roster];
 
 export default subcommands;
