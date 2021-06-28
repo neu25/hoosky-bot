1026fef9db3269450826e0ebbb9304f05c8269b6
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2021-06-28 09:01:03 -0400

    Allow optional user arg to get own pfp

--- src/commands/pfp.ts
+++ src/commands/pfp.ts
@@ -1,7 +1,6 @@
 import * as Discord from '../Discord';
 import Command from '../Command';
 import CommandOption from '../CommandOption';
-import { EmbedImage } from '../Discord';
 
 const pfp = new Command({
   name: 'pfp',
@@ -9,30 +8,37 @@ const pfp = new Command({
   options: [
     new CommandOption({
       name: 'user',
-      description: 'The user to get the profile picture of',
-      required: true,
+      description:
+        'The target user (leave blank to view your own profile picture)',
+      required: false,
       type: Discord.CommandOptionType.USER,
     }),
   ],
   handler: async ctx => {
-    const targetUserId = ctx.getArgument<string>('user') as string;
+    let targetUserId = ctx.getArgument<string>('user') as string;
+    if (!targetUserId) {
+      // With the `user` argument is omitted, get the executor's profile picture.
+      targetUserId = ctx.interaction.member?.user?.id ?? '';
+      if (!targetUserId) {
+        return ctx.respondWithError('Unable to identify user.');
+      }
+    }
 
     const user = await ctx.api.getUser(targetUserId);
-    let url;
-    // Info on image urls at https://discord.com/developers/docs/reference#image-formatting
-    if (user.avatar != null) {
+    let url: string;
+    // Info on image urls at https://discord.com/developers/docs/reference#image-formatting.
+    if (user.avatar !== null) {
       url = `https://cdn.discordapp.com/avatars/${targetUserId}/${user.avatar}.png?size=4096`;
     } else {
       url = `https://cdn.discordapp.com/embed/avatars/${
         Number(user.discriminator) % 5
       }.png?size=4096`;
     }
-    const image: EmbedImage = { url: url };
 
     return ctx.respondWithEmbed({
       type: Discord.EmbedType.RICH,
-      title: `Here is ${user.username}'s Profile Picture:`,
-      image: image,
+      title: `${user.username}'s Profile Picture:`,
+      image: { url },
     });
   },
 });
