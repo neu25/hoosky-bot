38bae26b99591c2834b0217de612a6ef7bacd612
Jay Sella <jay@jaysella.dev>
2021-07-28 10:31:20 -0400

    feat: add countdown show-schedule command

--- src/commands/countdown/subcommands/index.ts
+++ src/commands/countdown/subcommands/index.ts
@@ -2,7 +2,8 @@ import create from './create';
 import del from './delete';
 import list from './list';
 import setSchedule from './setSchedule';
+import showSchedule from './showSchedule';
 
-const subCommands = [create, del, list, setSchedule];
+const subCommands = [create, del, list, setSchedule, showSchedule];
 
 export default subCommands;
