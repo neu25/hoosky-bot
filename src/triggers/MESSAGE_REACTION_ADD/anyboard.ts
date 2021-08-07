ad3a610782bbbc2533d85e4f3ab9f01d44b3c17c
Aiyan Jiang <48393820+aiyan@users.noreply.github.com>
2021-08-07 15:24:39 -0400

    feat: tweak starboard

--- src/triggers/MESSAGE_REACTION_ADD/anyboard.ts
+++ src/triggers/MESSAGE_REACTION_ADD/anyboard.ts
@@ -14,7 +14,7 @@ export const generateHighlightMessageContent = (
 ): string => {
   return [
     bold(reaction.count.toLocaleString()),
-    EN_SPACE,
+    ' × ',
     emojiString,
     `${EN_SPACE}|${EN_SPACE}`,
     `<#${channelId}>`,
@@ -25,10 +25,10 @@ const anyboard = new Trigger({
   event: Discord.Event.MESSAGE_REACTION_ADD,
   handler: async ctx => {
     const {
-      user_id: reactorUserId,
+      user_id: ogUserId,
       guild_id: guildId,
-      channel_id: channelId,
-      message_id: messageId,
+      channel_id: ogChannelId,
+      message_id: ogMessageId,
       emoji,
     } = ctx.data;
     if (!guildId) {
@@ -36,25 +36,25 @@ const anyboard = new Trigger({
     }
 
     // Ignore the bot's own reactions.
-    if (reactorUserId === ctx.botUser.id) return;
+    if (ogUserId === ctx.botUser.id) return;
 
     // Format emoji to a consistent representation consumable by the Discord UI.
     const formattedEmoji = formatEmoji(emoji);
 
     // Get the original message.
-    const message = await ctx.api.getChannelMessage(channelId, messageId);
-    const { author, referenced_message: referencedMessage } = message;
-    const reactions = message.reactions ?? [];
+    const ogMessage = await ctx.api.getChannelMessage(ogChannelId, ogMessageId);
+    const { author, referenced_message: referencedMessage } = ogMessage;
+    const ogReactions = ogMessage.reactions ?? [];
 
     // Get the specific reaction type on the message matching the emoji added.
-    const reactionForEmoji = reactions.find(r => r.emoji.name === emoji.name);
+    const reactionForEmoji = ogReactions.find(r => r.emoji.name === emoji.name);
     if (!reactionForEmoji) return;
 
     // Try to get this board message. If this is `null`, then it means this message
     // hasn't been highlighted yet.
     const boardMsg = await ctx
       .anyboardMessages()
-      .getByMessageId(guildId, messageId);
+      .getByMessageId(guildId, ogMessageId);
     if (boardMsg) {
       // Reaction count hasn't changed, do nothing.
       if (boardMsg.reactionCount === reactionForEmoji.count) return;
@@ -64,14 +64,14 @@ const anyboard = new Trigger({
         boardMsg.highlightMessageId,
         {
           content: generateHighlightMessageContent(
-            channelId,
+            ogChannelId,
             reactionForEmoji,
             formattedEmoji,
           ),
         },
       );
 
-      await ctx.anyboardMessages().update(guildId, messageId, {
+      await ctx.anyboardMessages().update(guildId, ogMessageId, {
         reactionCount: reactionForEmoji.count,
       });
 
@@ -84,12 +84,15 @@ const anyboard = new Trigger({
       .get<AnyboardConfig>(guildId, Config.ANYBOARD);
     if (!anyboardCfg?.channelId || !anyboardCfg?.minReactionCount) return;
 
+    // If the reaction is in the anyboard channel, do nothing.
+    if (anyboardCfg.channelId === ogChannelId) return;
+
     const fields: Discord.EmbedField[] = [
       {
         name: 'Original',
         value: hyperlink(
           'Jump to message',
-          messageLink(guildId, channelId, messageId),
+          messageLink(guildId, ogChannelId, ogMessageId),
         ),
       },
     ];
@@ -120,28 +123,28 @@ const anyboard = new Trigger({
 
       const highlightMsg = await ctx.api.createMessage(anyboardCfg.channelId, {
         content: generateHighlightMessageContent(
-          channelId,
+          ogChannelId,
           reactionForEmoji,
           formattedEmoji,
         ),
         embeds: [
           {
             fields,
-            description: message.content,
+            description: ogMessage.content,
             author: {
               name: authorName,
               icon_url: authorAvatarUrl(author),
             },
-            timestamp: message.timestamp,
+            timestamp: ogMessage.timestamp,
             color: Discord.Color.WARNING,
             type: Discord.EmbedType.RICH,
           },
-          ...transformAttachmentsToEmbeds(message.attachments),
+          ...transformAttachmentsToEmbeds(ogMessage.attachments),
         ],
       });
 
       await ctx.anyboardMessages().create(guildId, {
-        _id: messageId,
+        _id: ogMessageId,
         userId: author.id,
         highlightMessageId: highlightMsg.id,
         highlightChannelId: highlightMsg.channel_id,
@@ -149,11 +152,11 @@ const anyboard = new Trigger({
       });
 
       // React to the message with the emoji.
-      await ctx.api.createReaction(
-        highlightMsg.channel_id,
-        highlightMsg.id,
-        formattedEmoji,
-      );
+      // await ctx.api.createReaction(
+      //   highlightMsg.channel_id,
+      //   highlightMsg.id,
+      //   formattedEmoji,
+      // );
 
       return;
     }
